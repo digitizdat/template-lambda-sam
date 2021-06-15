90905ebd61e143af79fef36eacba7cb99cf54356
Martin McGreal <mmmcgrea@amazon.com>
2021-06-15 11:32:49 -0500

    Initial commit

---
+++ {{cookiecutter.application_name}}/lambda_function.py
@@ -0,0 +1,17 @@
+import logging
+import os
+
+import jsonpickle
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+
+def lambda_handler(event, context):
+    logger.info(
+        "## ENVIRONMENT VARIABLES\r" + jsonpickle.encode(dict(**os.environ))
+    )
+    logger.info("## EVENT\r" + jsonpickle.encode(event))
+    logger.info("## CONTEXT\r" + jsonpickle.encode(context))
+
+    return event
